de25bf9c91f6a715ee7e178debfc2407b62ffe78
Matryoshika <mad.matryoshika@gmail.com>
2017-06-20 20:43:51 +0200

    Updating Registration to show lambda-expression alternatives

--- src/main/java/com/example/RenderAndRegister/BlockRegistry.java
+++ src/main/java/com/example/RenderAndRegister/BlockRegistry.java
@@ -1,27 +1,27 @@
 package com.example.RenderAndRegister;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import net.minecraft.block.Block;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
-import net.minecraftforge.event.RegistryEvent;
-import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.event.RegistryEvent.Register;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.fml.common.registry.GameRegistry;
 
 
 /**
- * Here we will assign blocks to an ArrayList, and then use said list to
+ * Here we will assign blocks to a collection, and then use said collection to
  * register, and later, also render each block.
  * 
  * The same functionality can be used for Items.
  * 
  * Please do note the Annotation below here. It is required because we will
- * be subscribing to an event, before preInit.
+ * be subscribing to an event, <b>before</b> preInit.
  */
-@Mod.EventBusSubscriber
+@EventBusSubscriber
 public class BlockRegistry {
 	
 	public static Block TESTBLOCK;
@@ -29,11 +29,11 @@ public class BlockRegistry {
 	
 	//We create a list that holds each and every block we make. When we make a new block, 
 	//we only need to add the block to this list.
-	public static List<Block> BlockList = new ArrayList<Block>();
+	public static Set<Block> blocks = new HashSet<Block>();
 	
 	public static void prepareBlocks(){
 		//Using the second constructor we made in the TestBlock class.
-		BlockList.add(TESTBLOCK = new TestBlock());
+		blocks.add(TESTBLOCK = new TestBlock());
 
 	}
 	
@@ -41,16 +41,20 @@ public class BlockRegistry {
 	//Forge calls it -for- us, when the RegistryEvent happens. This is why
 	//we had to use the @Mod.EventBusSubscriber at the top of the class.
 	@SubscribeEvent
-	public static void registerBlocks(RegistryEvent.Register<Block> event){
+	public static void registerBlocks(Register<Block> event){
 		
 		//We make sure that the list gets filled with our blocks.
 		prepareBlocks();
 		
 		//For-each-loop. Essentially, for each and every block we find in the BlockList, 
 		//we will call it "block", and register it.
-		for(Block block : BlockList){
+		for(Block block : blocks){
 			event.getRegistry().register(block);
 		}
+		
+		//OR we can also use Java8 Lambda expressions here.
+		//Only use either the for-loop, or the forEach expression. Not both (as that would register it twice)
+		blocks.forEach(event.getRegistry()::register);
 	}
 	
 	//Don't forget to register the ItemBlock variant (When held, or in an inventory, a block is an "item")
@@ -58,12 +62,26 @@ public class BlockRegistry {
 	//Thus, our registerBlocks method has already happened.
 	//ItemBlocks are of course not required to be made for Items.
 	@SubscribeEvent
-	public static void registerItemBlocks(RegistryEvent.Register<Item> event){
-		for(Block block : BlockList){
+	public static void registerItemBlocks(Register<Item> event){
+		
+		for(Block block : blocks){
 			ItemBlock iblock = new ItemBlock(block);
 			iblock.setRegistryName(block.getRegistryName());
 			event.getRegistry().register(iblock);
 		}
+		
+		
+		//OR we can do this in one line (we split it in two here, to make it easier to read all at once, but the split is only syntactical sugar)
+		// by using another lambda expression again
+		//What is done here is: for each block in this collection(that contains blocks! As infered by the type-parameter aka the <Block> part), 
+		//create a new ItemBlock and set it's RegistryName to the block's RegistryName. 
+		//Put this new ItemBlock into a new list, and for each thing in this new list, register
+		blocks.stream().map(block -> new ItemBlock(block).setRegistryName(block.getRegistryName()))
+		.collect(Collectors.toList()).forEach(event.getRegistry()::register);
+		
+		
+		//Choose one way, and stick to it. For-loops are simpler, but Lambda's are good to use, as they can simplify several lines of code into a single one.
+		
 	}
 
 }
--- src/main/java/com/example/RenderAndRegister/RenderRegistry.java
+++ src/main/java/com/example/RenderAndRegister/RenderRegistry.java
@@ -5,42 +5,55 @@ import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.item.Item;
 import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.client.model.ModelLoader;
-import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 /**
- * Just as we did with the BlockRegistry, we have to use the annotation 
- * to tell Forge "HEY, when you are ready to register models, use this!"
+ * Just as we did with the BlockRegistry, we have to use the annotation to tell
+ * Forge "HEY, when you are ready to register models, use this!"
  */
-@Mod.EventBusSubscriber
+@EventBusSubscriber
 public class RenderRegistry {
-	
-	
+
 	@SubscribeEvent
-	public static void registerBlockRenderer(ModelRegistryEvent event){
-		//Time for another for-loop. Like last time, we will loop
-		//through the same loop we used to register the blocks
-		for(Block block : BlockRegistry.BlockList){
-			
-			/**
-			 * DO NOT USE Minecraft.getMinecraft().getRenderItem().getItemModelMesher().register(Item.getItemFromBlock(block), 0, new ModelResourceLocation(block.getRegistryName(), "inventory"));
-			 * It is *incredibly* prone to issues. The only reason Vanilla rendering works, is because of the extensive knowledge of the system, and the years
-			 * of expertice the coders of Mojang have.
-			 * 
-			 * Instead, use this; It is provided by forge, and has to be called in before or during preInit: V
-			 */
+	public static void registerBlockRenderer(ModelRegistryEvent event) {
+
+		/**
+		 * DO NOT USE
+		 * Minecraft.getMinecraft().getRenderItem().getItemModelMesher().
+		 * register(Item.getItemFromBlock(block), 0, new
+		 * ModelResourceLocation(block.getRegistryName(), "inventory")); It is
+		 * *incredibly* prone to issues. The only reason Vanilla rendering
+		 * works, is because of the extensive knowledge of the system, and the
+		 * years of expertice the coders of Mojang have.
+		 * 
+		 * Instead, use ModelLoader; It is provided by forge, and has to be
+		 * called before or during preInit: V
+		 */
+
+		// Time for another for-loop. Like last time, we will loop
+		// through the same loop we used to register the blocks
+		for (Block block : BlockRegistry.blocks) {
 			ModelLoader.setCustomModelResourceLocation(Item.getItemFromBlock(block), 0, new ModelResourceLocation(block.getRegistryName(), "inventory"));
-			
-			/**
-			 * If you use modid + ":" + getUnlocalizedName().substring() for the ModelResourceLocation, I swear I will find you, and
-			 * bash your head with 1000 printed papers of this page.
-			 * It is *completely* redundant, moronic, atrocious and gratuitous to manually create the ModelResourceLocation, when
-			 * getRegistryName() DOES THAT FOR YOU! IT CREATES THE PATH USING MODID:NAME! THATS ALL YOU NEED!
-			 * 
-			 * Not to mention, unLocalizedName, should be used SOLELY to render the NAME of the item/block. It should not in any damned way
-			 * have anything to do with unique identifiers.
-			 * </rage>
-			 */
 		}
+
+		// OR we can use Lambda Expressions here as well
+		BlockRegistry.blocks.forEach(block -> ModelLoader.setCustomModelResourceLocation(Item.getItemFromBlock(block),0, new ModelResourceLocation(block.getRegistryName(), "inventory")));
+
+		
+		//Remember, only use either one, never both.
+		
+		/**
+		 * If you use modid + ":" + getUnlocalizedName().substring() for the
+		 * ModelResourceLocation, I swear I will find you, and bash your head
+		 * with 1000 printed papers of this page. It is *completely* redundant,
+		 * moronic, atrocious and gratuitous to manually create the
+		 * ModelResourceLocation, when getRegistryName() DOES THAT FOR YOU! IT
+		 * CREATES THE PATH USING MODID:NAME! THATS ALL YOU NEED!
+		 * 
+		 * Not to mention, unLocalizedName, should be used SOLELY to render the
+		 * NAME of the item/block. It should not in any damned way have anything
+		 * to do with unique identifiers. </rage>
+		 */
 	}
 }
