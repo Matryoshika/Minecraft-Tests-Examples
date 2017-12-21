9785369e47708e229b886937e352242ccd18d42a
Matryoshika <mad.matryoshika@gmail.com>
2017-12-21 20:18:21 +0100

    Update ClientProxy.java

--- src/main/java/com/example/RenderAndRegister/ClientProxy.java
+++ src/main/java/com/example/RenderAndRegister/ClientProxy.java
@@ -2,7 +2,7 @@ package com.example.RenderAndRegister;
 
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
-public class ClientProxy extends CommonProxy{
+public class ClientProxy extends ServerProxy{
 	
 	
 	public void preInit(FMLPreInitializationEvent event){
